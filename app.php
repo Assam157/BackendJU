a1e7f412e3b7706189e0eca1913a58fb564ad9ff
Assam157 <asasm8106@gmail.com>
2024-11-20 19:04:43 +0530

    Update app.php

--- app.php
+++ app.php
@@ -316,10 +316,12 @@ $app->add(function ($request, $handler) {
     return $handler->handle($request);
 });
 
-// MongoDB connection
-$mongoClient = new MongoDB\Client(
+ $mongoClient = new MongoDB\Client(
     "mongodb+srv://Maitreya:killdill12@cluster0.sk6ugig.mongodb.net/?retryWrites=true&w=majority",
-    [],
+    [
+        'ssl' => true,           // Explicitly enable SSL
+        'ssl_ca_certs' => '/path/to/cacert.pem'  // Path to CA certificate file if needed
+    ],
     [
         'typeMap' => [
             'root' => 'array',
